6a59350ed12a52cf696fb068f5ac45ab2dd64d32
Anesu Mafuvadze <am579@duke.edu>
2018-04-04 19:51:14 -0400

    Add gpio protol to skeleton

--- gpio_protocol.v
+++ gpio_protocol.v
@@ -1,4 +1,11 @@
 module gpio_protocol (GPIO, CLOCK_50, clock);
+	/*
+	[15:0] 	MESSAGE_DATA
+	[16]		SHARED_CLOCK	
+	
+	
+	*/
+	
 	inout [35:0]	GPIO;
 	input 			CLOCK_50;
 	output 			clock;
@@ -7,7 +14,8 @@ module gpio_protocol (GPIO, CLOCK_50, clock);
 	
 	wire 				LEADER,
 						FOLLOWER;
-							
+	
+	// Leader selection						
 	assign LEADER 	 = 0;
 	assign FOLLOWER = 1;
 	
@@ -16,5 +24,10 @@ module gpio_protocol (GPIO, CLOCK_50, clock);
 		else fpga_state = LEADER;
 	end
 	
-	assign GPIO[30] = fpga_state ? 1'bz : 1'b1; 
+	// Pin assignments for leader
+	assign GPIO[30] = fpga_state ? 1'bz : 1'b1; 		// Indicate leader chosen
+	assign GPIO[16] = fpga_state ? 1'bz : CLOCK_50; // Set shared clock
+	
+	assign clock = GPIO[16];
+	
 endmodule
